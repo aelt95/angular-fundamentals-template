199d95c8b295d2f42595d52e6e26b68dbd3202f1
aelt95 <anthonylopez1230@gmail.com>
2024-10-16 11:57:50 +0200

    Course form + duration pipe

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,19 +1,61 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import {
-  FormBuilder, FormGroup
-} from '@angular/forms';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-course-form',
-  templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss'],
+  selector: "app-course-form",
+  templateUrl: "./course-form.component.html",
+  styleUrls: ["./course-form.component.scss"],
 })
 export class CourseFormComponent {
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
-  courseForm!: FormGroup;
+  courseForm: FormGroup = new FormGroup({
+    title: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    description: new FormControl("", [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    author: new FormControl("", [Validators.minLength(2)]),
+    duration: new FormControl("", [Validators.required, Validators.min(0)]),
+  });
+  authors: string[] = [];
+  courseAuthors: string[] = [];
+  submitted = false;
+
+  onCreateAuthor() {
+    if (this.courseForm.get("author")?.valid) {
+      this.authors.push(this.courseForm.get("author")?.value);
+      this.courseForm.get("author")?.reset();
+    }
+  }
+
+  onAddAuthor(author: string) {
+    this.courseAuthors.push(author);
+    this.authors = this.authors.filter((authorName) => authorName !== author);
+  }
+
+  onDeleteAuthor(author: string, type?: string) {
+    if (type === "list") {
+      this.authors = this.authors.filter((authorName) => authorName !== author);
+    } else {
+      this.courseAuthors = this.courseAuthors.filter(
+        (authorName) => authorName !== author
+      );
+      this.authors.push(author);
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.courseForm.valid, this.authors);
+  }
   // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
 }
--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,19 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: 'duration'
+  name: "duration",
 })
-export class DurationPipe {
-    // Add your code here
+export class DurationPipe implements PipeTransform {
+  transform(duration: number): string {
+    if (!duration) {
+      return "00:00 hours";
+    }
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+    const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const hourLabel = hours === 1 ? "hour" : "hours";
+
+    return `${formattedHours}:${formattedMinutes} ${hourLabel}`;
+  }
 }
