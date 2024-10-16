8cb8abf9fc42081daed120f083a263531c25cbbe
aelt95 <anthonylopez1230@gmail.com>
2024-10-16 12:03:12 +0200

    Shared module include on course-info module, custom-date pipe

--- src/app/features/course-info/course-info.module.ts
+++ src/app/features/course-info/course-info.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CourseInfoComponent } from "./course-info.component";
+import { SharedModule } from "@app/shared/shared.module";
 const components = [CourseInfoComponent];
 
 @NgModule({
   declarations: [components],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [components],
 })
 export class CourseInfoModule {}
